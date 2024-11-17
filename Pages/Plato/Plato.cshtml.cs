23cca976c73d1ebaa1b017d8cb39017e80a2acd5
estevan franco abadia <drago2062@gmail.com>
2024-11-16 19:12:41 -0500

    pages plato

---
+++ Pages/Plato/Plato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStockWeb.Pages.Plato
+{
+    public class PlatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
