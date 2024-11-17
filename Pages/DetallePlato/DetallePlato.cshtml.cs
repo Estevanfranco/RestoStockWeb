f313560d5815874013a4f96ae7cd3218bbd0e2b8
estevan franco abadia <drago2062@gmail.com>
2024-11-16 19:25:32 -0500

    DetallePlato

---
+++ Pages/DetallePlato/DetallePlato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStockWeb.Pages.DetallePlato
+{
+    public class DetallePlatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
