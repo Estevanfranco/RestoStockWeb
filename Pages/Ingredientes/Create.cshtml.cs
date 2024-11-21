9cd67131401bd8effc72a5dd832b2a2057fac25e
Jose Leonardo Soto <jsotto2003@gmail.com>
2024-11-20 19:51:29 -0500

    Index pagina ingredientes CRUD

---
+++ Pages/Ingredientes/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStockWeb.Pages.Ingredientes
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
