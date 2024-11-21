5eb1d6b86cfb4c6c65802a3b56768b3ef445e980
Jose Leonardo Soto <jsotto2003@gmail.com>
2024-11-20 20:13:26 -0500

    Create pagina ingredientes CRUD

--- Pages/Ingredientes/Create.cshtml.cs
+++ Pages/Ingredientes/Create.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Razor;
+using RestoStockWeb.Models;
+using RestoStockWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestoStockWeb.Pages.Ingredientes
 {
-    public class Index1Model : PageModel
+    public class IngCreateModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private readonly RestoStockContext _context;
+
+		public IngCreateModel(RestoStockContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+
+		public Ingrediente Ingrediente { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Ingredientes == null || Ingrediente == null)
+			{
+				return Page();
+			}
+			_context.Ingredientes.Add(Ingrediente);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
 }
