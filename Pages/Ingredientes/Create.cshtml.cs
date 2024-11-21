852efa3f0213c3eef5d3bf9514f9a8a113618d12
Jose Leonardo Soto <jsotto2003@gmail.com>
2024-11-20 21:34:20 -0500

    Edit pagina ingredientes CRUD

--- Pages/Ingredientes/Create.cshtml.cs
+++ Pages/Ingredientes/Create.cshtml.cs
@@ -1,40 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Razor;
-using RestoStockWeb.Models;
 using RestoStockWeb.Data;
-using Microsoft.EntityFrameworkCore;
+using RestoStockWeb.Models;
 
 namespace RestoStockWeb.Pages.Ingredientes
 {
     public class IngCreateModel : PageModel
     {
-		private readonly RestoStockContext _context;
+        private readonly RestoStockContext _context;
 
-		public IngCreateModel(RestoStockContext context)
-		{
-			_context = context;
-		}
+        public IngCreateModel(RestoStockContext context)
+        {
+            _context = context;
+        }
 
-		public IActionResult OnGet()
-		{
-			return Page();
-		}
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
 
-		[BindProperty]
+        [BindProperty]
+        public Ingrediente ingrediente { get; set; } = default!;
 
-		public Ingrediente Ingrediente { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Ingredientes == null || ingrediente == null)
+            {
+                //return Page();
+            }
 
-		public async Task<IActionResult> OnPostAsync()
-		{
-			if (!ModelState.IsValid || _context.Ingredientes == null || Ingrediente == null)
-			{
-				return Page();
-			}
-			_context.Ingredientes.Add(Ingrediente);
-			await _context.SaveChangesAsync();
+            _context.Ingredientes.Add(ingrediente);
+            await _context.SaveChangesAsync();
 
-			return RedirectToPage("./Index");
-		}
-	}
+            return RedirectToPage("./Index");
+        }
+    }
 }
